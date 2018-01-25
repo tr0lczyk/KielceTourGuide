ef9481f72a7ca40dcf339a42c4f2320a5156c0d5
tr0lczyk <matt.olczyk@gmail.com>
2018-01-25 16:18:20 +0100

    Item object further edition

--- app/src/main/java/com/example/android/kielcetourguide/Item.java
+++ app/src/main/java/com/example/android/kielcetourguide/Item.java
@@ -5,4 +5,44 @@ package com.example.android.kielcetourguide;
  */
 
 public class Item {
+
+
+    /*
+    *
+    * States of the Item object
+    *
+    * */
+    private String mPlaceName;
+
+    private String mPlaceDescription;
+
+    private int mImageResourceId;
+
+    /*
+    *
+    * Creation of the Item object
+    *
+    * */
+    public Item(String placeName, String placeDescription, int imageResourceId){
+        mPlaceName = placeName;
+        mPlaceDescription = placeDescription;
+        mImageResourceId = imageResourceId;
+    }
+
+    /*
+    *
+    * @Methods of the Item object
+    *
+    * */
+    public String getPlaceName(){
+        return mPlaceName;
+    }
+
+    public String getPlaceDescription(){
+        return mPlaceDescription;
+    }
+
+    public int getImageResourceId(){
+        return mImageResourceId;
+    }
 }
